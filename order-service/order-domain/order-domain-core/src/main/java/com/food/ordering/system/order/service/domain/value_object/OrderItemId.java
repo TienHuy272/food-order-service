4bf682edca72439287b15d08f2091fe8e6e8691f
HuyNT <tienhuylionel@gmail.com>
2025-08-19 23:41:34 +0700

    add entity value_object order service domain

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/value_object/OrderItemId.java
@@ -0,0 +1,9 @@
+package com.food.ordering.system.order.service.domain.value_object;
+
+import com.food.ordering.system.domain.value_object.BaseId;
+
+public class OrderItemId extends BaseId<Long> {
+    public OrderItemId(Long value) {
+        super(value);
+    }
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/value_object/TrackingId.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.domain.value_object;
+
+import com.food.ordering.system.domain.value_object.BaseId;
+
+import java.util.UUID;
+
+public class TrackingId extends BaseId<UUID> {
+
+    public TrackingId(UUID value) {
+        super(value);
+    }
+}
